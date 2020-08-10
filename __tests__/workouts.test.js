27f3c09fbc8c3ab25fdcb393c32cedf0cc2d0064
JoLeaper <joleap123@gmail.com>
2020-08-10 16:07:08 -0700

    patch route created, test passing

--- __tests__/workouts.test.js
+++ __tests__/workouts.test.js
@@ -1,4 +1,5 @@
 const { agent } = require('../db/data-helpers');
+const Workout = require('../lib/models/Workout');
 
 describe('workout-list routes', () => {
   it('can create a new workout', () => {
@@ -27,17 +28,70 @@ describe('workout-list routes', () => {
     
   });
 
-  // it('get all workouts', () => {
-    
-  // });
+  it('get all workouts', async() => {
+    await Workout.create({
+      name: 'Bench Press',
+      muscles: 'Chest, Triceps',
+      description: 'Raise the bar up, and lower it in a controlled motion onto your chest. Once it touches, push (with an emphasis on using your chest and triceps). For an added challenge: pause when the bar touches your chest.',
+      reps: 25,
+      weight: 135
+    });
 
-  // it('can update a workout', () => {
-    
-
-  // });
-  // it('can delete a workout', () => {
-    
-  // });
-
+    await Workout.create({
+      name: 'Back Squat',
+      muscles: 'Core, Glutes, Quads',
+      description: 'Lift the bar onto your traps and walk out. When you are stable, lower yourself in a controlled motion while sticking your glutes out. Lower yourself until your hips dip beneath your knees, then rise up.',
+      reps: 25,
+      weight: 225
+    });
   
+    return agent
+      .get('/api/v1/workouts')
+      .then(res => {
+        expect(res.body).toHaveLength(2);
+      });
+      
+  });
+
+  it('get all workouts', async() => {
+    const workout1 = await Workout.create({
+      name: 'Bench Press',
+      muscles: 'Chest, Triceps',
+      description: 'Raise the bar up, and lower it in a controlled motion onto your chest. Once it touches, push (with an emphasis on using your chest and triceps). For an added challenge: pause when the bar touches your chest.',
+      reps: 25,
+      weight: 135
+    });
+
+    const updatedworkout1 = {
+      name: 'Bench Press',
+      muscles: 'Chest, Triceps',
+      description: 'Raise the bar up, and lower it in a controlled motion onto your chest. Once it touches, push (with an emphasis on using your chest and triceps). For an added challenge: pause when the bar touches your chest.',
+      reps: 25,
+      weight: 155
+    };
+
+    return agent
+      .patch(`/api/v1/workouts/${workout1._id}`)
+      .send(updatedworkout1)
+      .then(res => {
+        expect(res.body).toEqual({
+          _id: workout1.id,
+          name: 'Bench Press',
+          muscles: 'Chest, Triceps',
+          description: 'Raise the bar up, and lower it in a controlled motion onto your chest. Once it touches, push (with an emphasis on using your chest and triceps). For an added challenge: pause when the bar touches your chest.',
+          reps: 25,
+          weight: 155,
+          __v: 0
+        });
+      });
+      
+  });
 });
+
+// it('can update a workout', () => {
+    
+
+// });
+// it('can delete a workout', () => {
+    
+// });
