502e433421e4f2bf3250fc2de5790f8ec792ebbc
JoLeaper <joleap123@gmail.com>
2020-08-10 22:51:52 -0700

    added mongodb functionality to tests

--- __tests__/workouts.test.js
+++ __tests__/workouts.test.js
@@ -1,7 +1,25 @@
 const { agent } = require('../db/data-helpers');
 const Workout = require('../lib/models/Workout');
+const { MongoMemoryServer } = require('mongodb-memory-server');
+const mongod = new MongoMemoryServer();
+const mongoose = require('mongoose');
+const connect = require('../lib/utils/connect');
 
 describe('workout-list routes', () => {
+  beforeAll(async() => {
+    const uri = await mongod.getUri();
+    return connect(uri);
+  });
+
+  beforeEach(() => {
+    return mongoose.connection.dropDatabase();
+  });
+
+  afterAll(async() => {
+    await mongoose.connection.close();
+    return mongod.stop();
+  });
+  
   it('can create a new workout', () => {
     const newWorkout = {
       name: 'Bench Press',
